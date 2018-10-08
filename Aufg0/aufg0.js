e0b29d58d812d56798d136278008bd4c51d30a9d
obiwan1337 <43846856+obiwan1337@users.noreply.github.com>
2018-10-09 04:47:04 +0900

    Add files via upload

--- Aufg0/aufg0.js
+++ Aufg0/aufg0.js
@@ -1,18 +1,15 @@
 /*Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
-namespace aufg0{
-
+var aufg0;
+(function (aufg0) {
     function name() {
-        let input : string = prompt("Whats your name?");
-        let txt : string;
-
+        var input = prompt("Whats your name?");
+        var txt;
         txt = "Hello there, ";
         txt += input;
         txt += ". It is nice to meet you.";
-        document.getElementById("main").innerHTML+= txt;
+        document.getElementById("main").innerHTML += txt;
         console.log(input);
     }
-
     document.addEventListener('DOMContentLoaded', name);
-
-}
+})(aufg0 || (aufg0 = {}));
