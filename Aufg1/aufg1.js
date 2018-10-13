0cfa70b53d1d221ab9a97f725350545fa1c62682
obiwan1337 <43846856+obiwan1337@users.noreply.github.com>
2018-10-13 22:31:46 +0900

    Update aufg1.js

--- Aufg1/aufg1.js
+++ Aufg1/aufg1.js
@@ -1,42 +1,41 @@
-var n = 5;
-var c;
-var x = 0;
-var y = 0;
-for (var i = 0; i < n; i++) {
-    y += (i == 2) ? 20 : 50;
-    x = (x + 170) % 400;
-    switch (i) {
-        case 0:
-            c = "#ff0000";
-            break;
-        case 1:
-        case 4:
-            c = "#00ff00";
-            break;
-        case 3:
-            continue;
-        default:
-            c = "#0000ff";
+var Boxes;
+(function (Boxes) {
+    let n = 5;
+    let c;
+    let x = 0;
+    let y = 0;
+    for (let i = 0; i < n; i++) {
+        y += (i == 2) ? 20 : 50;
+        x = (x + 170) % 400;
+        switch (i) {
+            case 0:
+                c = "#ff0000";
+                break;
+            case 1:
+            case 4:
+                c = "#00ff00";
+                break;
+            case 3:
+                continue;
+            default:
+                c = "#0000ff";
+        }
+        for (let a = 50; a > 0; a -= 20) {
+            placeDiv(c, x, y, a, a);
+            if (i == 4)
+                break;
+        }
     }
-    for (var a = 50; a > 0; a -= 20) {
-        placeDiv(c, x, y, a, a);
-        if (i == 4)
-            break;
+    function placeDiv(_color, _x, _y, _width, _height) {
+        let div = document.createElement("div");
+        document.body.appendChild(div);
+        let s = div.style;
+        s.border = "thin solid black";
+        s.position = "absolute";
+        s.backgroundColor = _color;
+        s.width = _width + "px";
+        s.height = _height + "px";
+        s.left = _x + "px";
+        s.top = _y + "px";
     }
-}
-function placeDiv(_color, _x, _y, _width, _height) {
-    var div = document.createElement("div");
-    document.body.appendChild(div);
-    var s = div.style;
-    s.border = "thin solid black";
-    s.position = "absolute";
-    s.backgroundColor = _color;
-    s.width = _width + "px";
-    s.height = _height + "px";
-    s.left = _x + "px";
-    s.top = _y + "px";
-}
-//Erstellen Sie eine TraceTable zu untenstehendem TypeScript-Code und parallel dazu Aktivitätsdiagramme, die den Algorithmus beschreiben.
-//Stellen Sie die zu erwartende Ausgabe grafisch auf Papier dar.
-//Bringen Sie schließlich den Code zum Laufen und überprüfen Sie das Ergebnis.
-//Hinweis: Ein Konzept ist für diese Aufgabe nicht erforderlich.
+})(Boxes || (Boxes = {}));
