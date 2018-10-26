3609550ddf3c296c68b42d0a01ae81e456912eaa
obiwan1337 <43846856+obiwan1337@users.noreply.github.com>
2018-10-26 21:28:41 +0900

    Add files via upload

---
+++ Aufg3/js/aufg3.ts
@@ -0,0 +1,52 @@
+namespace Aufg3 {
+
+  let Deckarray: String[] = ["r0", "r1", "r1", "r2", "r2", "r3", "r3", "r4", "r4", "r5", "r5", "r6", "r6", "r7", "r7", "r8", "r8", "r9", "r9", "r<=>", "r<=>", "rx", "rx", "r+2", "r+2",
+    "y0", "y1", "y1", "y2", "y2", "y3", "y3", "y4", "y4", "y5", "y5", "y6", "y6", "y7", "y7", "y8", "y8", "y9", "y9", "y<=>", "y<=>", "yx", "yx", "y+2", "y+2",
+    "b0", "b1", "b1", "b2", "b2", "b3", "b3", "b4", "b4", "b5", "b5", "b6", "b6", "b7", "b7", "b8", "b8", "b9", "b9", "b<=>", "b<=>", "bx", "bx", "b+2", "b+2",
+    "g0", "g1", "g1", "g2", "g2", "g3", "g3", "g4", "g4", "g5", "g5", "g6", "g6", "g7", "g7", "g8", "g8", "g9", "g9", "g<=>", "g<=>", "gx", "gx", "g+2", "g+2",
+    "s+4", "s+4", "s+4", "s+4", "sw", "sw", "sw", "sw"]
+
+  function startgame(): void {
+    let o: string = prompt('Enter any value of cards');
+    let p: number;
+    p = Number(o);
+    for (let i: number = 0; i < p; i++) {
+
+      let randomnumber: number = Math.floor(Math.random() * Deckarray.length);
+      let color: string = Deckarray[randomnumber].substr(0, 1)
+      let number: string = Deckarray[randomnumber].substr(1)
+      starthand(color, number, i);
+      Deckarray.splice(randomnumber, 1);
+    }
+
+    function starthand(_color, _number, _i) {
+      switch (_color) {
+        case "r": {
+          _color == "#ff0000";
+        }
+        case "y": {
+          _color == "#ffff00";
+        }
+        case "b": {
+          _color == "#0000ff"
+        }
+        case "g": {
+          _color == "#228B22"
+        }
+      }
+      let canvas: HTMLCanvasElement = document.createElement("canvas");
+      canvas.setAttribute("id", "Karte" + _i);
+      document.getElementById("Karte" + _i).innerHTML += _number;
+      document.getElementById("hand").appendChild(canvas);
+      let ctx = canvas.getContext("2d");
+      ctx.font = "30px Ubuntu";
+      ctx.fillText("Hello World", 10, 50);
+      let s: CSSStyleDeclaration = canvas.style;
+      s.backgroundColor = "_color";
+
+
+    }
+  }
+  document.addEventListener('DOMContentLoaded', startgame);
+}
+
