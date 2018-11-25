991efdd6e92b44116a4f2967a9064b170caad9fc
obiwan1337 <43846856+obiwan1337@users.noreply.github.com>
2018-11-26 01:41:13 +0900

    Delete Daten.d.ts

--- Aufgabe4/js/Daten.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace cuztree {
-    let trees: articles[];
-    let stand: articles[];
-    let bulbs: articles[];
-    let candles: articles[];
-    let shipment: articles[];
-}
-//# sourceMappingURL=Daten.d.ts.map
