3731dbd40fcdf92b27201317b8dcdd16b007b64a
obiwan1337 <43846856+obiwan1337@users.noreply.github.com>
2018-12-15 01:01:02 +0900

    Add files via upload

---
+++ Aufg8/Client/DatabaseClient.js
@@ -0,0 +1,61 @@
+var DatabaseClient;
+(function (DatabaseClient) {
+    window.addEventListener("load", init);
+    //let serverAddress: string = "http://localhost:8100";
+    let serverAddress = "https://eier2.herokuapp.com/";
+    function init(_event) {
+        console.log("Init");
+        let insertButton = document.getElementById("insert");
+        let refreshButton = document.getElementById("refresh");
+        insertButton.addEventListener("click", insert);
+        refreshButton.addEventListener("click", refresh);
+    }
+    function insert(_event) {
+        let inputs = document.getElementsByTagName("input");
+        let query = "command=insert";
+        query += "&name=" + inputs[0].value;
+        query += "&firstname=" + inputs[1].value;
+        query += "&matrikel=" + inputs[2].value;
+        console.log(query);
+        sendRequest(query, handleInsertResponse);
+    }
+    function searchclicke(_event) {
+        let target = _event.target;
+        console.log("search testeru");
+        let matrikel = parseInt(target.value);
+        /*
+        let xhr: XMLHttpRequest = new XMLHttpRequest();
+        xhr.open("GET", serverAddress + "?command=search&matrikel=" + matrikel, true);
+        xhr.addEventListener("readystatechange", seamar);
+        xhr.send();
+ */
+    }
+    function seamar(_event) {
+    }
+    function refresh(_event) {
+        let query = "command=refresh";
+        sendRequest(query, handleFindResponse);
+    }
+    function sendRequest(_query, _callback) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", serverAddress + "?" + _query, true);
+        xhr.addEventListener("readystatechange", _callback);
+        xhr.send();
+    }
+    function handleInsertResponse(_event) {
+        let xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            alert(xhr.response);
+        }
+    }
+    function handleFindResponse(_event) {
+        let xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            let output = document.getElementsByTagName("textarea")[0];
+            output.value = xhr.response;
+            let responseAsJson = JSON.parse(xhr.response);
+            console.log(responseAsJson);
+        }
+    }
+})(DatabaseClient || (DatabaseClient = {}));
+//# sourceMappingURL=DatabaseClient.js.map
